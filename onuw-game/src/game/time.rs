69f56cac2f57e55fde5dc238d8c7791c17660c15
dorian451 <112524240+dorian451@users.noreply.github.com>
2023-12-07 23:57:59 -0500

    add an end time so agents know when the game is over

--- onuw-game/src/game/time.rs
+++ onuw-game/src/game/time.rs
@@ -1,9 +1,15 @@
 use crate::role::Role;
 
+use super::GamePlayer;
+
 #[derive(Debug)]
 pub enum ONUWTime<'a> {
     Dusk,
     Night(&'a dyn Role),
     Day,
     Vote,
+    End {
+        dead: &'a [GamePlayer],
+        winners: &'a [GamePlayer],
+    },
 }
