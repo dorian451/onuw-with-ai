bae2cdb921599e5eb04b256b77e5285f35801597
dorian451 <112524240+dorian451@users.noreply.github.com>
2023-12-07 23:57:46 -0500

    add more game errors

--- onuw-game/src/game/gamerror.rs
+++ onuw-game/src/game/gamerror.rs
@@ -4,4 +4,8 @@ use thiserror::Error;
 pub enum GameError {
     #[error("Excepted {} Roles due to having {players} players, but got {roles} Roles instead.", players + 3)]
     InvalidRoleCount { roles: usize, players: usize },
+    #[error("No more night actions")]
+    NoMoreNightActions,
+    #[error("Can't caculate dead players without votes!")]
+    WrongCmdOrder,
 }
